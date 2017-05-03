1080c5f1571c9c3de9258aa68d0ccb03e7e85e01
ChimiDEV <Tim.Walter1996@web.de>
2017-05-03 13:06:58 +0200

    WIP

--- Sin.c
+++ Sin.c
@@ -1,40 +1,23 @@
 #include <math.h>
 #include <stdio.h>
 
-long int factorial(int n) {
-    if(n < 0) {
-        //Error
-        printf("Error! Factorial of a negative number doesn't exist.");
-        return -1;
-    } else if(n == 0 || n == 1) {
-        return 1;
-    } else {
-        return n * factorial(n-1);
-    }
-}
-
 double power(double n, int m) {
-	double tmp = 1;
+	double result = 1;
 	int i;
-
-    // Power of n
-	for(i = 0; i < m; i++) {
-		tmp *= n;
-	}
-	return tmp;
+    // n to power of m
+    if (n == 0 || n == 1) {
+        // return 1
+        return result;
+    } else {
+        for(i = 0; i < m; i++) {
+    		result *= n;
+        }
+        return result;
+    }
 }
 
 double sin0(double x) {
     int i;
-
-    // Taylor Approximation (7)
-    /* for(i = 0; i < 7; i++) {
-        //double sign = power(-1, i);
-        sign = -sign;
-        double px = power(x, 2*i+1);
-        long fac = factorial(2*i+1);
-        result += sign * px / fac;
-    } */
     double numerator = x;
     double denominator = 1;
     double result = x;
--- Sin.s
+++ Sin.s
@@ -1,13 +1,37 @@
-Code:
+.data
+# Initialization of global data
+error: .asciiz "An error occured!"
+
+# Code:
     .globl main
     .text
 
 main:
-    # Register: $s0: Return Address of Main
+    # Register:
 
-factorial:
-    # Register: $a0: n
-    move     $t0, $a0        # t0 = a0
+
+power:
+    # Register: $a0: n, $a1: m, $v0: result, $t0: i (for-loop)
+    # n to power of m
+    li      $v0, 1              # load 1 to result
+    li      $t0, 1              # load 1 to i
+
+    li      $t1, 1                  # load 1 to t1 for if condition
+    beq     $a0, $zero, power_end   # if n == 0, then jump to end
+    beq     $a0, $t1, power_end     # or if n == 1, then jump to end
+
+power_loop:
+    blt     $t0, $a1, power_end     # if i < m, then jump to end
+    mult    $v0, $v0, $a0           # result = result * n
+    addi    $t0, t0, 1              # i++
+    j       power_loop              # jump to power_loop
+
+power_end:
+    jr      $ra                     # Jump back
+
+error:
+    la      $a0, error
+    li      $v0, 4
 
 done:
     move $a0, $s1       # Store sum for printing
